b836a428e457cbea026e0b1d9165dd12a471b9e2
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-12-17 23:10:56 +0530

    add gameboard module

--- main.js
+++ main.js
@@ -2,9 +2,35 @@ var gameBoardContainer = document.querySelector('.game-board-container')
 
 for(let i=0;i<9;i++){
     let gameTile = document.createElement('div')
+    gameTile.setAttribute('id', `tile${i}`)
     gameTile.classList.add('game-tile')
     gameBoardContainer.append(gameTile)
 }
 
 
 console.log(gameBoardContainer)
+
+
+const gameBoard = (() => {
+    var gameTileValues = []
+
+    const updateTileValue = (gameTile, newValue) => {
+
+        if(gameTileValues[gameTile.id])
+        return
+
+        gameTile.innerText = newValue
+        gameTileValues[gameTile.id] = newValue
+        showCurrentValues()
+    }
+
+    const showCurrentValues = () => console.log(gameTileValues)
+
+    return {updateTileValue, showCurrentValues}
+})()
+
+console.log(gameBoard)
+
+const gameTiles = document.querySelectorAll('.game-tile')
+console.log(gameTiles)
+gameTiles.forEach(gameTile => gameTile.addEventListener('click', () => gameBoard.updateTileValue(gameTile, 'X')))
