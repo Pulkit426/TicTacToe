c0800fcddacc8aba841102f05782fe48e5194e92
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-12-18 23:16:44 +0530

    add winner logic

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var gameBoardContainer = document.querySelector('.game-board-container')
 
 for(let i=0;i<9;i++){
     let gameTile = document.createElement('div')
-    gameTile.setAttribute('id', `tile${i}`)
+    gameTile.setAttribute('id', `${i}`)
     gameTile.classList.add('game-tile')
     gameBoardContainer.append(gameTile)
 }
@@ -12,16 +12,45 @@ console.log(gameBoardContainer)
 
 
 const gameBoard = (() => {
-    var gameTileValues = []
+    var gameTileValues = new Array(9).fill(null)
+    var currentChance = 'X'
 
-    const updateTileValue = (gameTile, newValue) => {
+    const updateTileValue = (gameTile) => {
 
         if(gameTileValues[gameTile.id])
         return
 
-        gameTile.innerText = newValue
-        gameTileValues[gameTile.id] = newValue
+        gameTile.innerText = currentChance
+        gameTileValues[gameTile.id] = currentChance
+        currentChance = currentChance==='X' ? 'O' : 'X'
         showCurrentValues()
+        checkForWinner()
+    }
+
+    const checkForWinner = () => {
+        if(gameTileValues[0] === gameTileValues[1] && gameTileValues[1] === gameTileValues[2])
+        alert("1Winner")
+
+        else if(gameTileValues[3] === gameTileValues[4] && gameTileValues[4] === gameTileValues[5])
+        alert("2Winner")
+
+        else if(gameTileValues[6] === gameTileValues[7] && gameTileValues[7] === gameTileValues[8])
+        alert("3Winner")
+
+        else if(gameTileValues[0] === gameTileValues[3] && gameTileValues[3] === gameTileValues[6])
+        alert("4Winner")
+
+        else if(gameTileValues[1] === gameTileValues[4] && gameTileValues[4] === gameTileValues[7])
+        alert("5Winner")
+
+        else if(gameTileValues[2] === gameTileValues[5] && gameTileValues[5] === gameTileValues[8])
+        alert("6Winner")
+
+        else if(gameTileValues[0] === gameTileValues[4] && gameTileValues[4] === gameTileValues[8])
+        alert("7Winner")
+
+        else if(gameTileValues[2] === gameTileValues[4] && gameTileValues[4] === gameTileValues[6])
+        alert("8Winner")
     }
 
     const showCurrentValues = () => console.log(gameTileValues)
@@ -33,4 +62,4 @@ console.log(gameBoard)
 
 const gameTiles = document.querySelectorAll('.game-tile')
 console.log(gameTiles)
-gameTiles.forEach(gameTile => gameTile.addEventListener('click', () => gameBoard.updateTileValue(gameTile, 'X')))
+gameTiles.forEach(gameTile => gameTile.addEventListener('click', () => gameBoard.updateTileValue(gameTile)))
