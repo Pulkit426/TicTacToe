e2ea623644bb48ef762bf075847d58ee080f86a3
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-12-19 18:41:16 +0530

    add game status and reset functionality

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var gameBoardContainer = document.querySelector('.game-board-container')
+let restartButton  = document.querySelector('.restart')
+
 
 for(let i=0;i<9;i++){
     let gameTile = document.createElement('div')
@@ -15,6 +17,25 @@ const gameBoard = (() => {
     var gameTileValues = new Array(9).fill(null)
     var currentChance = 'X'
 
+    const resetGame = () => {
+        gameTiles.forEach( gameTile => {
+            gameTile.innerText = ''
+        })
+        
+        gameTileValues.forEach( (tileValue,index) => {
+            gameTileValues[index]=null
+        })
+
+        currentChance = 'X'
+        updateGamestatus()
+    }
+
+    const updateGamestatus = () => {
+
+        let gameStatus = document.querySelector('.game-status')
+        gameStatus.innerText = `Player ${currentChance} Turn`
+    }
+
     const updateTileValue = (gameTile) => {
 
         if(gameTileValues[gameTile.id])
@@ -25,37 +46,39 @@ const gameBoard = (() => {
         currentChance = currentChance==='X' ? 'O' : 'X'
         showCurrentValues()
         checkForWinner()
+        updateGamestatus()
     }
 
     const checkForWinner = () => {
-        if(gameTileValues[0] === gameTileValues[1] && gameTileValues[1] === gameTileValues[2])
+        if(gameTileValues[0] === gameTileValues[1] && gameTileValues[1] === gameTileValues[2]==='X')
         alert("1Winner")
 
-        else if(gameTileValues[3] === gameTileValues[4] && gameTileValues[4] === gameTileValues[5])
+        else if(gameTileValues[3] === gameTileValues[4] && gameTileValues[4] === gameTileValues[5]==='X')
         alert("2Winner")
 
-        else if(gameTileValues[6] === gameTileValues[7] && gameTileValues[7] === gameTileValues[8])
+        else if(gameTileValues[6] === gameTileValues[7] && gameTileValues[7] === gameTileValues[8]==='X')
         alert("3Winner")
 
-        else if(gameTileValues[0] === gameTileValues[3] && gameTileValues[3] === gameTileValues[6])
+        else if(gameTileValues[0] === gameTileValues[3] && gameTileValues[3] === gameTileValues[6]==='X')
         alert("4Winner")
 
-        else if(gameTileValues[1] === gameTileValues[4] && gameTileValues[4] === gameTileValues[7])
+        else if(gameTileValues[1] === gameTileValues[4] && gameTileValues[4] === gameTileValues[7]==='X')
         alert("5Winner")
 
-        else if(gameTileValues[2] === gameTileValues[5] && gameTileValues[5] === gameTileValues[8])
+        else if(gameTileValues[2] === gameTileValues[5] && gameTileValues[5] === gameTileValues[8]==='X')
         alert("6Winner")
 
-        else if(gameTileValues[0] === gameTileValues[4] && gameTileValues[4] === gameTileValues[8])
+        else if(gameTileValues[0] === gameTileValues[4] && gameTileValues[4] === gameTileValues[8]==='X')
         alert("7Winner")
 
-        else if(gameTileValues[2] === gameTileValues[4] && gameTileValues[4] === gameTileValues[6])
+        else if(gameTileValues[2] === gameTileValues[4] && gameTileValues[4] === gameTileValues[6]==='X')
         alert("8Winner")
     }
 
     const showCurrentValues = () => console.log(gameTileValues)
 
-    return {updateTileValue, showCurrentValues}
+    updateGamestatus()
+    return {resetGame, updateTileValue, showCurrentValues}
 })()
 
 console.log(gameBoard)
@@ -63,3 +86,7 @@ console.log(gameBoard)
 const gameTiles = document.querySelectorAll('.game-tile')
 console.log(gameTiles)
 gameTiles.forEach(gameTile => gameTile.addEventListener('click', () => gameBoard.updateTileValue(gameTile)))
+
+restartButton.addEventListener('click', () => {
+    gameBoard.resetGame()
+})
